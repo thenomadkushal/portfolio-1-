8e8f6b960212d015b3183aff6a6e489b9849e553
kushaladlakha <kushaladlakha@outlook.com>
2022-02-13 00:13:11 +0530

    first commit

---
+++ function.js
@@ -0,0 +1,6 @@
+function isScriptAlreadyIncluded(src){
+    var scripts = document.getElementsByTagName("script");
+    for(var i = 0; i < scripts.length; i++) 
+       if(scripts[i].getAttribute('src') == src) return true;
+    return false;
+}
